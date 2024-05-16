63e733183b8cb11198ffc8dc2b3e39fe4803ba70
Rui Ferreira <ruif5970@gmail.com>
2024-05-16 23:50:04 +0100

    Added Layout

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import WhiteLogo from "./assets/logo-white.png"
+function Footer (){
+    return (
+        <footer>
+            <img src={WhiteLogo} alt="Logo" width="90px"/>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+function Nav (){
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About Us</a></li>
+                <li><a href="/">Reserve a table</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Nav;
