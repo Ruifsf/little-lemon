01424613fbcdca1b3a3a56d804bd24510f8327ed
Rui Ferreira <ruif5970@gmail.com>
2024-06-15 22:02:57 +0100

    Added style to Form

--- src/FormReserve.js
+++ src/FormReserve.js
@@ -10,7 +10,6 @@ function FormReserve ({availableTimes, setAvailableTimes, submitForm}) {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log("table requested");
         submitForm(formContent);
     }
 
@@ -26,34 +25,34 @@ function FormReserve ({availableTimes, setAvailableTimes, submitForm}) {
     return(
         <div className="formReserveContainer">
             <form onSubmit={handleSubmit} className="formReserve" id="form-component">
-                <h2>Reserve a Table</h2>
+                <h2 className="formH2">Reserve a Table</h2>
 
                 <div>
-                    <label htmlFor="datela">Choose a Date:</label> <br/>
-                    <input type="date" name="date" id="date" value={formContent.date} onChange={(e)=> setFormContent({date: e.target.value})} required></input>
+                    <label htmlFor="datela" className="formLabels">Choose a Date:</label> <br/>
+                    <input type="date" name="date" id="date" value={formContent.date} onChange={(e)=> setFormContent({date: e.target.value})} required className="formInputs"></input>
                 </div>
                 <div>
-                    <label htmlFor="time">Choose a Time:</label><br/>
-                    <select name="time" id="time" value={formContent.time} onChange={(e)=> setFormContent({time: e.target.value})}>
+                    <label htmlFor="time" className="formLabels">Choose a Time:</label><br/>
+                    <select name="time" id="time" value={formContent.time} onChange={(e)=> setFormContent({time: e.target.value})} className="formInputs">
                         {availableTimes.map((element) => {
                             return (<option value={element} key={element}>{element}</option>)
                         })}
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="number">Choose the number of people:</label><br/>
-                    <input type="number" placeholder="1" min="1" max="10" name="number" id="number" value={formContent.people} onChange={(e)=> setFormContent({people: e.target.value})}></input>
+                    <label htmlFor="number" className="formLabels">Choose the number of people:</label><br/>
+                    <input type="number" placeholder="1" min="1" max="10" name="number" id="number" value={formContent.people} onChange={(e)=> setFormContent({people: e.target.value})} className="formInputs"></input>
                 </div>
                 <div>
-                    <label htmlFor="occasion">Choose the Occasion:</label><br/>
-                    <select name="occasion" id="occasion" value={formContent.occasion} onChange={(e)=> setFormContent({occasion: e.target.value})}>
+                    <label htmlFor="occasion" className="formLabels">Choose the Occasion:</label><br/>
+                    <select name="occasion" id="occasion" value={formContent.occasion} onChange={(e)=> setFormContent({occasion: e.target.value})} className="formInputs">
                         <option value="Birthday">Birthday</option>
                         <option value="Business">Business</option>
                         <option value="Family">Family</option>
                         <option value="Other">Other</option>
                     </select>
                 </div>
-                <input type="submit" value="Request Table" disabled={disableSubmit} aria-label="On Click"></input>
+                <input type="submit" value="Request Table" disabled={disableSubmit} aria-label="On Click" className="formInputs"></input>
             </form>
         </div>
     );
